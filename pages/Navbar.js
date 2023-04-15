3dfae8e95fb32f190d7a0ca989b00b1996c9c68e
pavan1419 <ptechhat@gmail.com>
2023-04-15 19:34:49 +0530

    7:35 pm Saturday, 15 April 2023 (IST) Time in Hyderabad, Telangana

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -1,13 +1,17 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div>
       <nav className='bg-slate-900 text-white flex justify-between '>
-          <p className=" py-4 pl-5  ">Meet Pavan</p>
+        <p className=' py-4 pl-5  '>Meet Pavan</p>
         <ul className='pr-7 py-4 space-x-10 flex '>
           <li className='cursor-pointer'> Home </li>
           <li className='cursor-pointer'> About </li>
           <li className='cursor-pointer'> Contact Us </li>
-          <li className='cursor-pointer'> Catalog </li>
+          <li className='cursor-pointer'>
+            <Link href='/All_Link_Page/MyLinks'> My Links</Link>
+          </li>
         </ul>
       </nav>
     </div>
