fc5a69dd3d4f716cb282f91b563ffd905753326e
pavan1419 <ptechhat@gmail.com>
2023-04-15 19:34:18 +0530

    7:34 pm Saturday, 15 April 2023 (IST) Time in Hyderabad, Telangana

---
+++ pages/All_Link_Page/MyLinks.js
@@ -0,0 +1,28 @@
+export default function MyLinks() {
+  return (
+    <>
+      <div
+        className=' bg-fuchsia-950
+       shrink grid gap-4  p-4 mx-96 my-10 border rounded-3xl '>
+        <button className=' text-black bg-yellow-300 rounded-lg border-2 px-16 py-2 justify-self-center hover:bg-cyan-400 hover:text-black'>
+          Github
+        </button>
+        <button className='  text-black bg-yellow-300  rounded-lg border-2 px-16 py-2 justify-self-center hover:bg-cyan-400 hover:text-black'>
+          Linkedin
+        </button>
+        <button className='  text-black bg-yellow-300  rounded-lg border-2 px-16 py-2 justify-self-center hover:bg-cyan-400 hover:text-black'>
+          Netify
+        </button>
+        <button className='  text-black bg-yellow-300  rounded-lg border-2 px-16 py-2 justify-self-center hover:bg-cyan-400 hover:text-black'>
+          Blog
+        </button>
+        <button className='  text-black bg-yellow-300  rounded-lg border-2 px-16 py-2 justify-self-center hover:bg-cyan-400 hover:text-black'>
+          Pintresst
+        </button>
+        <button className='  text-black bg-yellow-300  rounded-lg border-2 px-16 py-2 justify-self-center hover:bg-cyan-400 hover:text-black'>
+          Projects
+        </button>
+      </div>
+    </>
+  );
+}
